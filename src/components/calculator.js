90c264e523acdd98a389f23bac614ac3c66716c2
krstjrk <kristel.juurik@gmail.com>
2021-09-27 21:02:06 +0300

    added buttons and bootstap rows-cols

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -4,8 +4,71 @@ import '../stylesheets/styles.scss';
 export default class Calculator extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="calculator-container">
+                <div className="display">Display Placeholder Text</div>
+                <div className="buttons-container">
+                    <div className="row">
+                        <div className="col-6">
+                            <button className="btn btn-outline-dark">Clear</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">/</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">x</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">7</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">8</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">9</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">-</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">4</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">5</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">6</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">+</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">1</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">2</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">3</button>
+                        </div>
+                        <div className="col-3">
+                            <button className="btn btn-outline-dark">.</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <button className="btn btn-outline-dark">0</button>
+                        </div>
+                        <div className="col-6">
+                            <button className="btn btn-outline-dark">=</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
