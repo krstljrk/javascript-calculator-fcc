da020ee283683d0c45fbe977072265a0b2a7d9a7
krstjrk <kristel.juurik@gmail.com>
2021-09-27 22:00:26 +0300

    finally got bootstrap styles down

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,67 +5,67 @@ export default class Calculator extends Component {
     render() {
         return (
             <div className="calculator-container">
-                <div className="display">Display Placeholder Text</div>
+                <div className="display text-center">Display Placeholder Text</div>
                 <div className="buttons-container">
-                    <div className="row">
-                        <div className="col-6">
-                            <button className="btn btn-outline-dark">Clear</button>
+                    <div className="row g-0">
+                        <div className="col-6 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">Clear</button>
                         </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">/</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">/</button>
                         </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">x</button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">7</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">8</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">9</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">-</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">x</button>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">4</button>
+                    <div className="row g-0">
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">7</button>
                         </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">5</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">8</button>
                         </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">6</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">9</button>
                         </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">+</button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">1</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">2</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">3</button>
-                        </div>
-                        <div className="col-3">
-                            <button className="btn btn-outline-dark">.</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">-</button>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <button className="btn btn-outline-dark">0</button>
+                    <div className="row g-0">
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">4</button>
                         </div>
-                        <div className="col-6">
-                            <button className="btn btn-outline-dark">=</button>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">5</button>
+                        </div>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">6</button>
+                        </div>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">+</button>
+                        </div>
+                    </div>
+                    <div className="row g-0">
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">1</button>
+                        </div>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">2</button>
+                        </div>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">3</button>
+                        </div>
+                        <div className="col-3 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">.</button>
+                        </div>
+                    </div>
+                    <div className="row g-0">
+                        <div className="col-6 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">0</button>
+                        </div>
+                        <div className="col-6 d-flex">
+                            <button className="btn btn-outline-dark flex-fill">=</button>
                         </div>
                     </div>
                 </div>
