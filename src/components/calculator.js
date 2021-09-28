e9f85746df08e6fc7ca8e683f23865714c87d910
krstjrk <kristel.juurik@gmail.com>
2021-09-28 18:36:55 +0300

    Added initialize method to Clear btn

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,18 +5,26 @@ export default class Calculator extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            displayText: "Placeholder text"
+            display: 'Something'
         }
+
+        this.initialize = this.initialize.bind(this);
+    }
+
+    initialize = () => {
+        this.setState({
+            display: ' '
+        })
     }
 
     render() {
         return (
             <div className="calculator-container">
-                <div className="display text-center">{this.state.displayText}</div>
+                <div className="display text-center">{this.state.display}</div>
                 <div className="buttons-container">
                     <div className="row g-0">
                         <div className="col-6 d-flex">
-                            <button className="btn btn-outline-dark flex-fill">Clear</button>
+                            <button className="btn btn-outline-dark flex-fill" onClick={this.initialize}>Clear</button>
                         </div>
                         <div className="col-3 d-flex">
                             <button className="btn btn-outline-dark flex-fill">/</button>
